4de7a15e3db0634c83e1dbecf7624f8b5d14b529
matjazsu <matjaz.suber@gmail.com>
2014-08-18 14:12:24 +0200

    Implement HMM.java and Viterbi.java

--- HMM/src/Hmm.java
+++ HMM/src/Hmm.java
@@ -7,6 +7,15 @@
  */
 public class Hmm {
 
+	/**
+	 * Private properties
+	 */
+	private int m_numStates;
+	private int m_numOutputs;
+	private double m_transitionProbability[][];
+	private double m_emisionProbability[][];
+	private int m_dummyStateIndex;
+	
     /** Constructs an HMM from the given data.  The HMM will have
      * <tt>numStates</tt> possible states and <tt>numOutputs</tt>
      * possible outputs.  The HMM is then built from the given set of
@@ -15,53 +24,117 @@ public class Hmm {
      * <tt>i</tt>-th state sequence, and similarly for
      * <tt>output[i][j]</tt>.
      */
-    public Hmm(int numStates, int numOutputs,
-	       int state[][], int output[][]) {
-
-	// your code here
-
+    public Hmm(int numStates, int numOutputs, int state[][], int output[][]) 
+    {
+       	this.m_numStates = numStates + 1; //numStates + dummy state
+    	this.m_numOutputs = numOutputs;
+    	this.m_dummyStateIndex = numStates - 1; //index for the dummy state
+    	
+    	//Initialize transition probability table
+    	this.m_transitionProbability = new double[this.m_numStates][this.m_numStates];
+    	
+    	//Initialize emision probability table
+    	this.m_emisionProbability = new double[this.m_numStates][this.m_numOutputs];
+    	
+    	//Build probabilities
+    	this.buildHMM(state, output);
     }
 
-    /** Returns the number of states in this HMM. */
-    public int getNumStates() {
-
-	// your code here
-
+    /** Returns the number of states in this HMM. 
+     *	Number of Hidden states
+     */
+    public int getNumStates() 
+    {
+    	return this.m_numStates;
     }
 
-    /** Returns the number of output symbols for this HMM. */
-    public int getNumOutputs() {
-
-	// your code here
-
+    /** Returns the number of output symbols for this HMM. 
+     *	Number of Observable states
+     */
+    public int getNumOutputs() 
+    {
+    	return this.m_numOutputs;
     }
 
     /** Returns the log probability assigned by this HMM to a
      * transition from the dummy start state to the given
      * <tt>state</tt>.
      */
-    public double getLogStartProb(int state) {
-
-	// your code here
-
+    public double getLogStartProb(int state) 
+    {
+    	return this.m_transitionProbability[this.m_dummyStateIndex][state];
     }
 
     /** Returns the log probability assigned by this HMM to a
      * transition from <tt>fromState</tt> to <tt>toState</tt>.
      */
-    public double getLogTransProb(int fromState, int toState) {
-
-	// your code here
-
+    public double getLogTransProb(int fromState, int toState) 
+    {
+    	return this.m_transitionProbability[fromState][toState];
     }
 
     /** Returns the log probability of <tt>state</tt> emitting
      * <tt>output</tt>.
      */
-    public double getLogOutputProb(int state, int output) {
-
-	// your code here
-
+    public double getLogOutputProb(int state, int output) 
+    {
+    	return this.m_emisionProbability[state][output];
+    }
+    
+    /**Fill the matrix m_transitionProbability, m_emisionProbability with computed probabilities
+     * Source code taken from: https://github.com/ravifreek63/Artificial-Intelligence-Projects/blob/master/Viterbi/Hmm.java
+     * Author: Ravi Tandon
+     * Modified by: Matjaz Suber
+     * Date: 18.08.2014
+     */
+    private void buildHMM(int state[][], int output[][])
+    {
+    	int currentState;
+    	
+    	int[][] transitionCount = new int[this.m_numStates][this.m_numStates];
+    	int[][] outputProbabilitiesCount = new int[this.m_numStates][this.m_numOutputs];
+    	int[] outputCount = new int[this.m_numStates];
+    	int[] transitionsState = new int[this.m_numStates];
+    	
+    	for(int row = 0; row < state.length; row++)
+    	{
+    		int lastState = this.m_dummyStateIndex;
+    		for(int column = 0; column < state[row].length; column++)
+    		{
+    			currentState = state[row][column];
+    			transitionCount[lastState][currentState] += 1;
+    			outputProbabilitiesCount[currentState][output[row][column]] += 1;
+    			transitionsState[lastState] += 1;
+    			outputCount[currentState] += 1;
+    			lastState = currentState;
+    		}
+    	}
+    	
+    	this.updateTransitionProbability(transitionCount, transitionsState);
+    	this.updateEmisionProbabilit(outputProbabilitiesCount, outputCount);
+    }
+    
+    private void updateTransitionProbability(int [][] transitionCount, int[] transitionState)
+    {
+    	for(int row = 0; row < this.m_numStates; row ++)
+    	{
+    		for(int column = 0; column < this.m_numStates - 1; column ++)
+    		{
+    			this.m_transitionProbability[row][column] = 
+    					Math.log((((double)transitionCount[row][column] + 1) / (transitionState[row] + this.m_numStates - 1)));
+    		}
+    	}
     }
 
+    private void updateEmisionProbabilit(int[][] outputProbabilitesCount, int[] outputCount)
+    {
+    	for(int row = 0; row < this.m_numStates - 1; row ++)
+    	{
+    		for(int column = 0; column < this.m_numOutputs; column ++)
+    		{
+    			this.m_emisionProbability[row][column] = 
+    					Math.log((((double)outputProbabilitesCount[row][column] + 1) / (outputCount[row] + this.m_numOutputs)));
+    		}
+    	}
+    }
 }
--- HMM/src/Viterbi.java
+++ HMM/src/Viterbi.java
@@ -4,14 +4,22 @@
  */
 public class Viterbi {
 
+	/**
+	 * Private properties
+	 */
+	private Hmm m_hmm;
+	private double m_bestPathValue[];
+	private int m_parentState[][];
+	private int m_numStates;
+	
     /** This is the constructor for this class, which takes as input a
      * given HMM with respect to which most likely sequences will be
      * computed.
      */
-    public Viterbi(Hmm hmm) {
-
-	// your code here
-
+    public Viterbi(Hmm hmm) 
+    {
+    	this.m_hmm = hmm;
+    	this.m_numStates = this.m_hmm.getNumStates();
     }
 
     /** Returns the most likely state sequence for the given
@@ -21,10 +29,89 @@ public class Viterbi {
      * state sequence should have the same number of elements as the
      * given output sequence.
      */
-    public int[] mostLikelySequence(int output[]) {
-
-	// your code here
-
+    public int[] mostLikelySequence(int output[]) 
+    {
+    	int[] mostLikelySequence = new int[output.length];
+    	this.m_bestPathValue = new double[this.m_numStates - 1];
+    	this.m_parentState = new int[this.m_numStates - 1][output.length];
+    	
+    	for(int time = 1; time < output.length; time++)
+    	{
+    		this.updateBestPathValue(output, time);
+    	}
+    	
+    	double maxValue = Double.NEGATIVE_INFINITY;
+    	int currentValueIndex = 0;
+    	for(int currentState = 0; currentState < this.m_hmm.getNumStates() - 1; currentState++)
+    	{
+    		double currentValue = this.m_bestPathValue[currentState];
+    		if(currentValue > maxValue)
+    		{
+    			currentValueIndex = currentState;
+    			maxValue = currentValue;
+    		}
+    	}
+    	
+    	int optimalStateIndex = currentValueIndex;
+    	for(int currentTimeIndex = output.length; currentTimeIndex > 0; currentTimeIndex--)
+    	{
+    		mostLikelySequence[currentTimeIndex - 1] = optimalStateIndex;
+    		optimalStateIndex = this.m_parentState[optimalStateIndex][currentTimeIndex - 1];
+    	}
+    	
+    	return mostLikelySequence;
     }
+    
+    /**Updates the bestPathValue variable and set m_parentState matrix
+     * Source code taken from: https://github.com/ravifreek63/Artificial-Intelligence-Projects/blob/master/Viterbi/Viterbi.java
+     * Author: Ravi Tandon
+     * Modified by: Matjaz Suber
+     * Date: 18.08.2014
+     */
+	private void updateBestPathValue(int[] output, int time) 
+	{
+		int currentOutput = output[time - 1];
+		int parrentIndex;
+		double[] tempPathValues = new double[this.m_numStates - 1];
+		
+		if(time == 1)
+		{
+			for(int currentState = 0; currentState < this.m_numStates - 1; currentState++)
+			{
+				this.m_bestPathValue[currentState] = (this.m_hmm.getLogStartProb(currentState)) + 
+						(this.m_hmm.getLogOutputProb(currentState, currentOutput));
+			}
+		}
+		else
+		{
+			for(int currentState = 0; currentState < this.m_numStates - 1; currentState++)
+			{
+				double maxValue = Double.NEGATIVE_INFINITY;
+				double pathValue;
+				parrentIndex = 0;
+				
+				for(int prevState = 0; prevState < this.m_numStates - 1; prevState++)
+				{
+					pathValue = this.m_bestPathValue[prevState] 
+							+ (this.m_hmm.getLogTransProb(prevState, currentState)) 
+							+ (this.m_hmm.getLogOutputProb(currentState, currentOutput));
+					
+					if(pathValue > maxValue)
+					{
+						maxValue = pathValue;
+						parrentIndex = prevState;
+					}
+				}
+				
+				tempPathValues[currentState] = maxValue;
+				this.m_parentState[currentOutput][time - 1] = parrentIndex;
+			}
+			
+			for(int currentState = 0; currentState < this.m_numStates - 1; currentState++)
+			{
+				this.m_bestPathValue[currentState] = tempPathValues[currentState];
+			}
+		}
+	}
 
 }
